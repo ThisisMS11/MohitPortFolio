36f0fec7b8c4dc70c43548112447bcc8d0dd93c4
MohitSaini <mailtomohit2002@gmail.com>
2023-01-16 15:09:21 +0530

    Added Project Card

--- public/index.js
+++ public/index.js
@@ -13,3 +13,39 @@ document.addEventListener('click', (e) => {
         // console.log(e.target.closest("[skillsdropdownmenu]").querySelector('.Allskills'));
     }
 })
+
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
